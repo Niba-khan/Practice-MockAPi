d4031e3e996076f68d6954ae9aa7cac8eb23df60
Niba farooq <164494729+Niba-khan@users.noreply.github.com>
2025-01-13 17:29:43 +0500

    practice

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,19 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => (
-  <nav className="flex justify-between items-center px-6 py-4 bg-cyan-300">
-    <div className="text-lg font-bold">NK,s Collection</div>
+  <nav className="flex justify-between items-center px-6 py-4 bg-cyan-300 mb-24">
+   <div className="flex items-center text-lg font-bold">
+  <Image
+    src='/image/logo.avif'
+    alt='logo'
+    width={100}
+    height={100}
+    className='rounded-full'
+  />
+  <span className="ml-2 text-2xl">Resturant</span>
+</div>
+
     <ul className="flex space-x-6">
       <li><Link href="/">Home</Link></li>
       <li><Link href="/about">About</Link></li>
